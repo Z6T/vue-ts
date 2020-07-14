630833139a598d49833071b963db6e5aeca3adc2
zhangtao15 <zhangtao15@xiaomi.com>
2020-07-14 17:55:10 +0800

    初体验

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      data:{{count}} <br/>
+      prop:{{msg}} <br/>
+      <button @click="addCount">+</button>
+  </div>
+</template>
+
+<script lang="ts" >
+import {Component,Prop,Vue} from 'vue-property-decorator'
+@Component
+export default  class HelloWorld extends Vue{
+    @Prop() private msg!: string;
+    private count = 669;
+    private addCount(){
+        this.count++
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <App msg="测试下ts的prop" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from '@/components/App'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components:{App}
 }
 </script>
